fc4ffea9bbf2b80c4a14273a70bcd664ef56b243
prawin3337 <prawin3337@gmail.com>
2024-03-10 15:06:09 +0530

    Added pending maintainance report API

--- index.ts
+++ index.ts
@@ -21,8 +21,8 @@ app.use((req: Request, res: Response, next: NextFunction) => {
 });
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
--- routes/maintenance.ts
+++ routes/maintenance.ts
@@ -3,10 +3,13 @@ import { ApiResponse } from "../modules/response.module";
 import { MaintenanceModule } from "../modules/maintenance.module";
 import { AuthModule } from "../modules/auth.module";
 import { Member } from "../modules/member.module";
+import { TransactionModule } from "../modules/transaction.module";
 
 const router = Router();
 const authModule = new AuthModule();
 const maintenanceModule = new MaintenanceModule();
+const members = new Member();
+const transactionModule = new TransactionModule();
 
 router.post('/calculate', authModule.authenticateToken, (req: any, res: Response) => {
     const {flatNo} = req.body;
@@ -32,8 +35,6 @@ router.post('/calculate/all', (req: any, res: Response) => {
         });
         return;
     }
-
-    const members = new Member();
 
     members.getMemberIds(async (err:any, data:any) => {
         if(err) {
@@ -97,5 +98,84 @@ router.get("/all", authModule.authenticateToken, (req: any, res: any, next: any)
             });
     }
 });
+
+router.get("/reports", authModule.authenticateToken, (req: any, res: any, next: any) => {
+    const { financYear } = req.query;
+    members.getMemberIds(async (err: any, result: any) => {
+        if(!err) {
+            const memrs: any[] = result.filter((obj: any) => obj.flatNo !== "0");
+            let maintainanceDetails:any = [];
+
+            // Add pending maintaincance details
+            await setPendingMaintaincanceDetails(memrs, financYear)
+                .then((data) => {
+                    maintainanceDetails = data
+                })
+                .catch((err) => {
+                    const apiRes = new ApiResponse(err, {});
+                    res.status(apiRes.statusCode).json(apiRes.data);
+                });
+
+            // Add last transaction details
+            await setTransactionDetails(maintainanceDetails)
+                .then()
+                .catch((err) => {
+                    const apiRes = new ApiResponse(err, {});
+                    res.status(apiRes.statusCode).json(apiRes.data);
+                });;
+
+            const apiRes = new ApiResponse(null, maintainanceDetails);
+            res.status(apiRes.statusCode).json(apiRes.data);
+        } else {
+            const apiRes = new ApiResponse(err, {});
+            res.status(apiRes.statusCode).json(apiRes.data);
+        }
+    })
+});
+
+async function setPendingMaintaincanceDetails(members: any[], financYear: any) {
+    let maintainanceDetails: any = [];
+    for (const member of members) {
+        const { flatNo } = member;
+        await maintenanceModule.getMentainance(flatNo, JSON.parse(financYear))
+            .then((data: any) => {
+                const penaltyData = data.filter(({ penaltyMonCnt, maintainanceAmt }: any) => {
+                    return (penaltyMonCnt > 0 && !maintainanceAmt)
+                });
+
+                const obj = {
+                    totalPenaltyAmt: 0,
+                    penaltyMonCnt: 0,
+                    flatNo: data[0].flatNo,
+                    pendingMaintainanceAmt: 0,
+                    totalPendingAmt: 0
+                };
+                penaltyData.forEach((d: any) => {
+                    obj.totalPenaltyAmt += d.penaltyAmt;
+                    obj.penaltyMonCnt = d.penaltyMonCnt;
+                    obj.pendingMaintainanceAmt = (1500 * penaltyData.length);
+                    obj.totalPendingAmt = (obj.pendingMaintainanceAmt + obj.totalPenaltyAmt);
+                });
+                maintainanceDetails.push(obj);
+            }).catch((err) => {
+                return Promise.reject(err);
+            });
+    }
+    return Promise.resolve(maintainanceDetails);
+}
+
+async function setTransactionDetails(maintainanceDetails: any[]): Promise<any> {
+    for (const tran of maintainanceDetails) {
+        await transactionModule.getAllMemberTransactions(tran.flatNo)
+            .then((data: any) => {
+                const d = data.filter((o: any) => o.isApproved === "y");
+                Object.assign(tran, d[d.length-1]);
+            })
+            .catch((err) => {
+                return Promise.reject(err);
+            });
+    }
+    return Promise.resolve(maintainanceDetails);
+}
 
 module.exports = router;
